929279bce9013fa01fb5b2d99c32a979ac72929e
gangzhidong <gangzidong@126.com>
2023-07-17 16:44:03 +0800

    add init code

--- main/main.go
+++ main/main.go
@@ -11,8 +11,8 @@ func main() {
 	cmdStr := strings.Join(os.Args, " ")
 	fmt.Printf("全部命令:%s \n", cmdStr)
 	fmt.Printf("命令:%s \n", os.Args[1])
-	fmt.Printf("参数:%s \n", strings.Join(os.Args[1:], " "))
-	cmd := exec.Command(os.Args[1], os.Args[1:]...)
+	fmt.Printf("参数:%s \n", strings.Join(os.Args[2:], " "))
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
